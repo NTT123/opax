b13a5524452e7a92f2401cce56398d20a30e3c1f
NTT123 <9960521+NTT123@users.noreply.github.com>
2021-10-10 16:34:16 +0000

    Fix dependence.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 __version__ = "0.2.1"
 url = "https://github.com/ntt123/opax"
 
-install_requires = ["pax >= 0.3.1"]
+install_requires = ["pax3 >= 0.3.1"]
 setup_requires = []
 tests_require = []
 
@@ -19,5 +19,5 @@ setup(
     tests_require=tests_require,
     packages=find_packages(exclude=["examples", "tests"]),
     extras_require={"test": tests_require},
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 )
