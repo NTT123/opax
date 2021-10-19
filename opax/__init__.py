081e232044b21ce160b70700a9a8e8aea67f0419
NTT123 <9960521+NTT123@users.noreply.github.com>
2021-10-19 15:28:41 +0000

    Import `add_decayed_weights`.
    Add `__all__`  to `__init__.py` file.

--- opax/__init__.py
+++ opax/__init__.py
@@ -1,9 +1,10 @@
 """PAX optimizer library."""
 
-from . import optimizer, schedule
+from . import schedule
 from .optimizer import adam, adamw, rmsprop, sgd
 from .transform import (
     GradientTransformation,
+    add_decayed_weights,
     chain,
     clip,
     clip_by_global_norm,
@@ -16,4 +17,26 @@ from .transform import (
 )
 from .utils import apply_gradients, apply_updates, transform_gradients
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
+
+__all__ = (
+    "adam",
+    "adamw",
+    "add_decayed_weights",
+    "apply_gradients",
+    "apply_updates",
+    "chain",
+    "clip_by_global_norm",
+    "clip",
+    "GradientTransformation",
+    "rmsprop",
+    "scale_by_adam",
+    "scale_by_rms",
+    "scale_by_schedule",
+    "scale_by_stddev",
+    "scale",
+    "schedule",
+    "sgd",
+    "trace",
+    "transform_gradients",
+)
